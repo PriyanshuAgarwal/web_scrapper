451dbff814c4fdb82fce7eaf51eb2871a2273981
Priyanshu Agarwal <priyanshu.agarwal@nowfloats.com>
2018-08-14 13:43:55 +0530

    Fix index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(__dirname + '/index.html');
+  res.sendFile(mainDir + '/views/index.html');
 });
 
 module.exports = router;
